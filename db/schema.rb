24ae2be56c75522f80b430460a443408b7406821
“samurai” <matsushita.samurai@gmail.com>
2018-08-18 16:28:39 +0000

    “tweet_app”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_134431) do
+ActiveRecord::Schema.define(version: 2018_08_18_160848) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_08_11_134431) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
